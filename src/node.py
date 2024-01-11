e60ec8cfd1a9d6ef1ccae7a00cbd209a0a85bb23
loic scoth <loic.scoth.etu@univ-lille.fr>
2024-01-11 11:31:33 +0100

    fin de séance

--- src/node.py
+++ src/node.py
@@ -6,9 +6,40 @@ class node() :
         self.kmin=kmin
         self.kmax=kmax
     
+    #def search(key) :
+        
+        
+        
+    def dicho(listKeys,key) :
+        t=len(listKeys)-1
+        d, f = 0, t
+        if (listKeys[f] == key) :
+            return (True,f)
+        elif (d < t and listKeys[d] == key) :
+            return (True, d)
+        
+        while (f-d > 1) :
+            m=(d+f)//2
+
+            if (listKeys[m]>=key):
+                f=m
+                if ( f< t and listKeys[f] == key) :
+                    return (True,f)
+            else :
+                d=m
+                if (d < t and listKeys[d] == key) :
+                    return (True, d)            
+            
+        return (False,0)
+    
+        
     #def getSizeNode() :
     #def getPos() :
     #def setNewChild() :
     #def removeChild() :
-    #def getSize() :
+        
+    def getSize(listKey) :
+        return len(listKey)
+    
+    
     #def isLeaf() :
