edd064aff3b58a9b2251634de7667f947dc55e44
loic scoth <loic.scoth.etu@univ-lille.fr>
2024-01-11 09:44:01 +0100

    debut projet

---
+++ src/BTree.py
@@ -0,0 +1,15 @@
+class Btree() :
+    
+    def __init__(self,size,string) :
+        self.size=size
+        self.string=string
+        
+    
+    
+    #def search() :
+    #def insertion():
+    #def delete() :
+    #def linear() :
+    #def createTree() :
+    #def height() :
+    #def isArbre() :
---
+++ src/node.py
@@ -0,0 +1,12 @@
+class node() :
+    
+    def __init__(self,size,keys,child,leaf) :
+        self.size=size
+        self.keys=keys
+        self.child=child
+        self.leaf=leaf
+    
+    #def getSize() :
+    #def getPos() :
+    #def setNew() :
+    #def 
