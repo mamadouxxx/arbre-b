9bed5275256baed5a9770cd79d8140b8a8aba497
loic scoth <loic.scoth.etu@univ-lille.fr>
2024-02-15 09:14:12 +0100

    ajout tests

--- src/BTree.py
+++ src/BTree.py
@@ -22,12 +22,19 @@ class Btree() :
     def insertion(self, value):
         """
         Exemple(s):
-        >>> Btree(2, Node([12, 42], [Node([2, 3]), Node([25]), Node([50])])).insertion(1)
+        >>> a =Btree(2, Node([12, 42], [Node([2, 3]), Node([25]), Node([50])]))
+        >>> a.insertion(1)
         True
-        >>> Btree(3, Node([12,25,50], [Node([1,11]), Node([20]), Node([100])])).insertion(10)
+        >>> a.search(1)
+        (Node([1]), 0)
+        >>> b= Btree(3, Node([12,25,50], [Node([1,11]), Node([20]), Node([100])]))
+        >>> b.insertion(10)
         True
+        >>> b.search(10)
+        (Node([10, 12]), 0)
         >>> Btree(2, Node([4, 10], [Node([1, 3]), Node([25]), Node([50])])).insertion(4)
         True
+        
         """
         fini, milieu, g, d = self.root.insert(value)
         if (not fini):
@@ -53,7 +60,9 @@ class Btree() :
     #def getSize () :
     #def changeRoot() :
     #def isEqual() :
-                
+    
+    def __repr__(self) :
+        return f"Btree({self.root})"
                 
 if __name__ == '__main__':
     import doctest
